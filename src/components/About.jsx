02836ec836f9ab086f66e327f35bcca386db3153
yogeshk39 <yogesh7k.2002@gmail.com>
2023-07-05 18:22:23 +0530

    adding files

---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+        Highly motivated and enthusiastic MERN stack web developer seeking an internship position to gain practical experience and contribute to the success
+         of a dynamic organization. Eager to apply my knowledge of web development technologies, data structures and algorithms, problem-solving skills, and 
+         creativity to create innovative and robust web solutions using the MERN stack.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import HeroImage from "../assets/heroImage.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I'm a Full Stack Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+          Passionate Full Stack Developer skilled in front-end (HTML, CSS, JavaScript) and back-end (Node.js, Python) development.
+           Committed to creating efficient, user-centric applications, integrating APIs, and staying updated with the latest technologies to deliver exceptional results.
+          </p>
+
+          <div>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <img
+            src={HeroImage}
+            alt="my profile"
+            className="rounded mx-auto w-full md:w-full ml-20"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
